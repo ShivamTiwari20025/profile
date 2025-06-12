f9b52745eb4765fd14a485da6b9b179b621ca45f
shivam.tiwari20025@gmail.com <shivam.tiwari20025@gmail.com>
2025-06-12 22:29:57 +0530

    jaio

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   preview: {
-    host: true, // equivalent to 0.0.0.0
+    host: true, // bind to 0.0.0.0
     port: process.env.PORT || 4173,
-    allowedHosts: ['profile-2-5pr0.onrender.com']
+    allowedHosts: ['*'] // allow all hosts
   }
 })
